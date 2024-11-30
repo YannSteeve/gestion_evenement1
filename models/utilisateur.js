109e676291eb1862fc5336c18eeadbc9bfcd7134
YannSteeve <mouendouyann08.gmail.com>
2024-11-30 06:29:01 +0100

    modelsutilsateur

---
+++ models/utilisateur.js
@@ -0,0 +1,22 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/db';
+
+const Utilisateur = sequelize.define('Utilisateur', {
+  nom: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  mot_de_passe: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  timestamps: true,
+});
+
+export default Utilisateur;
